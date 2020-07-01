9b1a3a758ff21260df1d25ca910aedd49393eb24
zhongjin <1341439190@qq.com>
2020-07-02 00:54:05 +0800

    2020.6
    Database project（ticket）

---
+++ ticket/src/ticket/util/ticutil.java
@@ -0,0 +1,19 @@
+package ticket.util;
+
+import java.sql.Connection;
+
+/**
+ * 数据库工具类
+ */
+public class ticutil {
+
+    private  String tUtil = "jdbc:mysql://localhost:3306/ticket";//数据库连接地址
+    private  String tUserName = "root";
+    private  String tPassward = "123456";
+    private  String jdbcName = "com.mysqk.Driver";
+
+
+    public Connection getCon()throws Exception{
+    
+    }
+}
